b8b467437cbd197756eb1dc54b04297f2f690469
Lucas Theis <ltheis@twitter.com>
2019-04-12 19:40:55 +0100

    Added rate limiter to results server

--- docker/results/results_server.py
+++ docker/results/results_server.py
@@ -4,6 +4,9 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from socketserver import ThreadingMixIn
 from sqltools import *
 from threading import Thread
+from time import time
+from rate_limit import RateLimiter, Bucket, Period as Per
+from collections import defaultdict
 import json
 import os
 import re
@@ -12,6 +15,20 @@ import ssl
 DB_URI = os.environ.get('DB_URI', 'sqlite:///clic2019.db')
 HTTP_PORT = int(os.environ.get('RESULTS_HTTP_PORT', 80))
 HTTPS_PORT = int(os.environ.get('RESULTS_HTTPS_PORT', 443))
+RATE_LIMIT_SECOND = int(os.environ.get('RATE_LIMIT_SECOND', 3))
+RATE_LIMIT_MINUTE = int(os.environ.get('RATE_LIMIT_SECOND', 20))
+
+
+def create_rate_limiter():
+	rate_of = Bucket.builder()
+	limiter = RateLimiter(
+		per_second=rate_of(RATE_LIMIT_SECOND, Per.SECOND),
+		per_minute=rate_of(RATE_LIMIT_MINUTE, Per.MINUTE))
+	return limiter
+
+
+# creates and stores a rate limiter per IP
+RATE_LIMITERS = defaultdict(create_rate_limiter)
 
 
 class SimpleServer(ThreadingMixIn, HTTPServer):
@@ -24,31 +41,37 @@ class Handler(BaseHTTPRequestHandler):
 		self.send_header('Content-type', 'text/plain')
 		self.end_headers()
 
+
 	def do_GET(self):
-		match = re.match('/(?P<task>transparent|lowrate)/(?P<phase>test|valid)/?', self.path)
+		if not RATE_LIMITERS[self.client_address[0]].reduce():
+			# too many requests
+			self.send_error(429)
 
-		if match is None:
-			self.send_error(404)
 		else:
-			phase = match.group('phase')
-			task = match.group('task')
+			match = re.match('/(?P<task>transparent|lowrate)/(?P<phase>test|valid)/?', self.path)
 
-			print('Connecting...')
-			db = db_connect()
+			if match is None:
+				self.send_error(404)
+			else:
+				phase = match.group('phase')
+				task = match.group('task')
 
-			print('Fetching results...')
-			results = db_get_results(db, task, phase)
+				print('Connecting...')
+				db = db_connect()
 
-			print('Sending headers...')
-			self._set_headers()
+				print('Fetching results...')
+				results = db_get_results(db, task, phase)
 
-			print('Sending content...')
-			self.wfile.write(b'window.leaderboard = ')
-			self.wfile.write(json.dumps(results).encode())
+				print('Sending headers...')
+				self._set_headers()
 
-			print('Closing connection...')
-			db.close()
-			print('Done.')
+				print('Sending content...')
+				self.wfile.write(b'window.leaderboard = ')
+				self.wfile.write(json.dumps(results).encode())
+
+				print('Closing connection...')
+				db.close()
+				print('Done.')
 
 	def do_HEAD(self):
 		self._set_headers()
